feb0998eddd2cd080b57d9c6a67d81cdfb02f0bc
ShaguftaAmreen <shaguftamuqeeeem2000@gmail.com>
2024-11-12 12:20:57 +0530

    Promise.all

--- promiseall/promiseall/src/App.js
+++ promiseall/promiseall/src/App.js
@@ -1,39 +1,42 @@
-import './App.css';
+import "./App.css";
+import PromiseAll from "./PromiseAll";
 
 function App() {
+  // const promise1 = Promise.resolve("Amreen");
+  // const promise2 = 42;
+  // const promise3 = new Promise((resolve, reject) => {
+  //   resolve("Promise has resolved.");
+  // });
 
-  const promise1=Promise.resolve("Amreen");
-  const promise2=42;
-  const promise3=new Promise((resolve,reject)=>{resolve("Promise has resolved.")});
- 
+  // const promise4 = new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     let error = false;
+  //     if (!error) {
+  //       resolve({ userName: "Amreen", passWord: "1234" });
+  //     } else {
+  //       reject("ERROR : Something went wrong!");
+  //     }
+  //   }, 1000);
+  // });
+  // promise4.then((res) => {
+  //   console.log(res);
+  // });
+  // promise4.catch((err) => {
+  //   console.log(err);
+  // });
 
-  const promise4=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-      let error=false;
-      if(!error){
-        resolve({userName:"Amreen",passWord:"1234"})
-      }
-      else{
-        reject("ERROR : Something went wrong")
-      }
-    },1000)
-  })
-  promise4.then((res)=>{
-    console.log(res)
-  })
-  promise4.catch((err)=>{
-    console.log(err)
-  })
-
-
-Promise.all([promise1,promise2,promise3]).then((res)=>{
-  console.log(res)
-})
-.catch((err)=>{ console.log("ERROR : ",err)})
+  // Promise.all([promise1, promise2, promise3])
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((err) => {
+  //     console.log("ERROR : ", err);
+  //   });
 
   return (
     <div className="App">
-      
+      <h2>Promise.all</h2>
+      <PromiseAll />
     </div>
   );
 }
---
+++ promiseall/promiseall/src/PromiseAll.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const PromiseAll = () => {
+  const [data, setData] = useState([]);
+
+  const [getData, setGetData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const promise1 = axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const promise2 = axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const [data1, data2] = await Promise.all([promise1, promise2]);
+
+        setData(data1.data);
+        setGetData(data2.data);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  // console.log(data);
+  // console.log(getData);
+
+  return (
+    <div>
+      {data.map((ele) => (
+        <span>{ele.name} &nbsp; &nbsp;</span>
+      ))}
+      <br />
+      <h3>
+        {getData.map((ele) => (
+          <span>{ele.id} &nbsp; &nbsp;</span>
+        ))}
+      </h3>
+    </div>
+  );
+};
+
+export default PromiseAll;
+
+//   useEffect(() => {
+//     const promise1 = fetch("https://jsonplaceholder.typicode.com/users", {
+//       method: "GET",
+//     });
+//     const promise2 = fetch("https://jsonplaceholder.typicode.com/posts", {
+//       method: "GET",
+//     });
+//     Promise.all([promise1, promise2])
+//       .then((res) => {
+//         console.log("promise all response", res);
+//         for (let i = 0; i < res.length; i++) {
+//           res[i].json().then((data) => {
+//             setData(data);
+//             setGetData(data);
+//           });
+//         }
+// //   console.log(res)
+//      setData(res[0].data);
+//      setGetData(res[1].data);
+//       })
+//       .catch((err) => {
+//         console.log("Error");
+//       });
+//   }, []);
+
+//Leanne Graham    Ervin Howell    Clementine Bauch    Patricia Lebsack  
+//Chelsey Dietrich    Mrs. Dennis Schulist    Kurtis Weissnat  
+//Nicholas Runolfsdottir V    Glenna Reichert    Clementina DuBuque    
