97b3b4f75b03f046cd86e5e8eb4c416aa82591bb
ShaguftaAmreen <shaguftamuqeeeem2000@gmail.com>
2024-11-14 19:12:51 +0530

    new commit

---
+++ backend/script.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const script = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default script
--- react-query/react-query/src/App.js
+++ react-query/react-query/src/App.js
@@ -1,44 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 function App() {
   const userData = useQuery({
-    queryKey: ['users'],
+    queryKey: ["users"],
     queryFn: () => {
-      return fetch('https://jsonplaceholder.typicode.com/users')
-        .then((response) => response.json());
+      return fetch("https://jsonplaceholder.typicode.com/users").then(
+        (response) => response.json()
+      );
     },
     enabled: false,
   });
 
   const mutatePost = useMutation({
-    mutationKey: ['posts'],
+    mutationKey: ["posts"],
     mutationFn: (newPost) => {
-      return fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
+      return fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
         body: JSON.stringify(newPost),
         headers: {
-          'Content-type': 'application/json; charset=UTF-8',
+          "Content-type": "application/json; charset=UTF-8",
         },
-      })
-        .then((response) => response.json());
+      }).then((response) => response.json());
     },
   });
 
-console.log("hello world",mutatePost.data)
+  console.log("hello world", mutatePost.data);
 
-let [title,setTitle]=useState("")
-let [body,setBody]=useState("")
-let [userId,setUserId]=useState("")
+  let [title, setTitle] = useState("");
+  let [body, setBody] = useState("");
+  let [userId, setUserId] = useState("");
 
-
-
-const handleClick=()=>{
-  mutatePost.mutate({ title, body, userId })
-}
-//() => mutatePost.mutate({ title: 'First Post', body: 'First Post Body', userId: 1 })
+  const handleClick = () => {
+    mutatePost.mutate({ title, body, userId });
+  };
+  //() => mutatePost.mutate({ title: 'First Post', body: 'First Post Body', userId: 1 })
   return (
     <div>
       <div>
@@ -46,21 +44,45 @@ const handleClick=()=>{
         <div>
           {userData.isFetching && <div>Fetching user data...</div>}
           {userData.isError && <div>{`Error getting data!!!`}</div>}
-          {userData.data && userData.data.length > 0 && userData.data.map((user) => (
-            <div key={user.id}>{user.name}</div>
-          ))}
+          {userData.data &&
+            userData.data.length > 0 &&
+            userData.data.map((user) => <div key={user.id}>{user.name}</div>)}
         </div>
       </div>
       <hr />
       <div>
+        <input
+          type="text"
+          value={title}
+          name="title"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          value={body}
+          name="body"
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          value={userId}
+          name="userId"
+          onChange={(e) => {
+            setUserId(e.target.value);
+          }}
+        />
+        <br />
+        <br />
 
-        <input type='text' value={title} name="title" onChange={(e)=>{setTitle(e.target.value)}}/><br /><br />
-        <input type='text' value={body} name='body' onChange={(e)=>{setBody(e.target.value)}}/><br /><br />
-        <input type='text' value={userId} name="userId" onChange={(e)=>{setUserId(e.target.value)}}/><br /><br />
-        
-        <button onClick={handleClick}>
-          Add New Post
-        </button>
+        <button onClick={handleClick}>Ad d New Post</button>
         <div>
           {mutatePost.isLoading && <div>Adding new post...</div>}
           {mutatePost.isError && <div>{`Error adding new post!!!`}</div>}
@@ -75,14 +97,6 @@ const handleClick=()=>{
 
 export default App;
 
-
-
-
-
-
-
-
-
 //************************************************
 // import React from 'react';
 // //import logo from './logo.svg';
@@ -92,11 +106,11 @@ export default App;
 
 // function App() {
 //   const userData = useQuery({
-//     queryKey: ['users'], 
+//     queryKey: ['users'],
 //     queryFn: () => {
 //       return fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json());
 //     },
-//     enabled: false, 
+//     enabled: false,
 //   });
 
 //   return (
